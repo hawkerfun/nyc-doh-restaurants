e098762d36fbe8bac76379e91c05e91e59cb3c49
Sergei Khazov <sergei.khazov@blinkfitness.com>
2018-09-22 16:51:15 -0400

    adjust styles and layout

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,23 +3,31 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
   MatButtonModule, 
-  MatToolbarModule
+  MatToolbarModule,
+  MatTableModule,
+  MatProgressSpinnerModule
 } from '@angular/material';
 import {MatIconModule} from '@angular/material/icon';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { TableSearchComponent } from './table-search/table-search.component';
+import { EtlControlComponent } from './etl-control/etl-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableSearchComponent,
+    EtlControlComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/src/app/etl-control/etl-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-etl-control',
+  templateUrl: './etl-control.component.html',
+  styleUrls: ['./etl-control.component.css']
+})
+export class EtlControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
